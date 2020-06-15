1bcf370a8f80237c1459eb0f16d410f813fa4ae0
Jeff Adler <jeffm.adler@gmail.com>
2020-06-15 19:34:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -51,14 +51,14 @@ def game_hash
           :slam_dunks => 5
         },
         {
-          :player_name => "Jason Terry",
-          :number => 31,
-          :shoe => 15,
-          :points => 9,
-          :rebounds => 2,
-          :assists => 2,
-          :steals => 4,
-          :blocks => 11,
+          :player_name => "Jason Terry"
+          :number => 31
+          :shoe => 15
+          :points => 9
+          :rebounds => 2
+          :assists => 2
+          :steals => 4
+          :blocks => 11
           :slam_dunks => 1
         }
       ]
